9744ba9482279f4084cbfa8b77c338efb99f2b7c
Serhii Onishchuk <onixsonic@gmail.com>
2019-07-07 22:10:30 +0300

    Search logic without refactoring

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -16,22 +16,35 @@ class SearchService < ApplicationService
   def search
     content = @data.split("},")
     content[0] = ''
-    result_of_search = content.select { |c| /#{query_validation}/i.match?(c) }
-    exclude_validation.length.times do |index|
-      result_of_search = result_of_search.reject { |c| /#{exclude_validation[index]}/i.match?(c) } if exclude_validation.size > 0
+    content[-1] = content.last[0..-4]
+    query_validation.length.times do |index|
+      content.select! { |c| /#{query_validation[index]}/i.match?(c) }
     end
+    result_of_search = exclude_validation(content)
     return [{ 'text_message': 'Not found' }] if result_of_search.empty?
     json_string = result_of_search.size > 1 ? "[" + result_of_search.join("},") + "}]" : "[" + result_of_search.join() + "}]"
     JSON.parse(json_string)
   end
 
   def query_validation
-    @query.match(/\w+/)
+    @query.split(' ')
   end
 
-  def exclude_validation
-    words = @query.split(' ')
-    words = words.select { |w| w[0] == '-' }
-    words.join(" ").gsub("-", "").split(" ")
+  def test(query, content)
+    result_of_search = []
+    query.length.times do |index|
+      content.reject! { |c| /#{query[index]}/i.match?(c) }
+    end
+    content
+  end
+
+  def exclude_validation(search_values)
+    words = @query.split(' ').select { |w| w[0] == '-' }
+    words = words.join(" ").gsub("-", "").split(" ")
+    return search_values if words.size == 0
+    words.length.times do |index|
+      search_values = search_values.reject { |c| /#{words[index]}/i.match?(c) } 
+    end
+    search_values
   end
 end
